d751805d2b46e6d003493ae10242504895f0d598
Kimi Weng <kimiweng@Kimis-MacBook-Pro.local>
2023-01-14 19:15:56 -0500

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      Hellow World
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/services.js
@@ -0,0 +1,2 @@
+import http from "./http-common";
+import axios from 'axios';
