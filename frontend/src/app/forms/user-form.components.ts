3d3b7004825b6197a2c57b2191aa981bc4983ee8
jespieee <mhuhu@umich.edu>
2023-02-05 18:38:57 -0500

    adding frontend changes

--- frontend/src/app/forms/user-form.components.ts
+++ frontend/src/app/forms/user-form.components.ts
@@ -1,9 +1,9 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild, AfterViewInit, OnInit } from '@angular/core';
+import { ExperienceFormComponent } from '../experience-form/experience-form.component';
 import { User } from '../user';
 import { Service } from 'src/app/services/service';
 import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 import { Validators } from '@angular/forms';
-
 
 @Component({
     selector: 'app-user-form',
@@ -12,29 +12,55 @@ import { Validators } from '@angular/forms';
 })
 
 export class UserFormComponent {
+    submitted = false;
     studentStatuses = ['Freshman', 'Sophomore', 'Junior', 'Senior', 'Graduate'];
 
     skillForm = this.fb.array([
         this.fb.control('')
     ])
 
+    public experiences: FormArray;
     public experienceForm: FormGroup;
 
     userForm = this.fb.group({
+        url: [''],
         fullName: ['', Validators.required],
         gradeLevel: [''],
         major: [''],
-        skills: this.skillForm,
+        skills: this.skillForm
     })
 
+    constructor(private service: Service, private fb: FormBuilder) { 
+        this.experienceForm = this.fb.group({
+            experiences: this.fb.array([this.createExperience()])
+        });
+    }
+        
+    createExperience(): FormGroup {
+        return this.fb.group({
+            title: '',
+            company: '',
+            startDate: '',
+            endDate: '',
+            description: '',
+        });
+      }
     
-    constructor(private service: Service, private fb: FormBuilder) { }
+      get experienceControls() {
+        return this.experienceForm.get('experiences')['controls'];
+      }
     
+      addExperience(): void {
+        this.experiences = this.experienceForm.get('experiences') as FormArray;
+        this.experiences.push(this.createExperience());
+      }
     
+      removeExperience(i: number): void {
+        this.experiences.removeAt(i);
+      }
     // name, student status, major, skills, experience
-    model = new User('', '', '', {}, {});
     
-    submitted = false;
+    
 
     get userFormData() {
         return this.userForm as FormGroup;
@@ -42,6 +68,7 @@ export class UserFormComponent {
     get skills(): FormArray {
         return this.userForm.get('skills') as FormArray;
     }
+    
     addSkill() {
         this.skills.push(this.fb.control(''));
     }
@@ -51,63 +78,21 @@ export class UserFormComponent {
     onSubmit() { 
         this.submitted = true; 
         console.log("Made it here");
-<<<<<<< HEAD
-        const testData = {
-            "url": "https://www.linkedin.com/jobs/search/?currentJobId=3339978993&f_JT=I&keywords=software%20engineer%20intern",
-            "name": "Kimi Weng",
-            "gradeLevel": "Sophomore",
-            "major": "Computer Science",
-            "skills": {
-                "1": "C++",
-                "2": "Python",
-                "3": "Javascript",
-                "4": "Unreal Engine",
-                "5": "Rust",
-                "6": "SQL",
-                "7": "Docker",
-                "8": "AWS"
-            },
-            "experiences": {
-                "1": {
-                    "title": "MES Software Engineer Intern",
-                    "company": "Yanfeng Automotive Interior Systems Co.",
-                    "onGoing": false,
-                    "startDate": "May 2022",
-                    "endDate": "Jul 2022",
-                    "description": "Self proposed and led a team of two to develop a web application for parsing multiple 1000+ lines of on-site MES engineers’ troubleshooting logs and generating statistical reports with graphics with Javascript and React.js, which greatly increased the efficiency of the employees. Showcased the product in a department-wide presentation and hosted the application on the company network. Troubleshooting company proprietary manufacturing software with SQL on-site to maintain production line uptime."
-                },
-                "2": {
-                    "title": "Research Programmer",
-                    "company": "Department of Nuclear Engineering, University of Michigan",
-                    "onGoing": true,
-                    "startDate": "Sep 2022",
-                    "endDate": "n/a",
-                    "description": "Developing AR software for displaying 3D visualizations of radiation signals with C++, Rust, and Unity C#. Iterating on the Marching Cube algorithm that triangulates 3D point cloud data structure and generates a 3D mesh representation that could be displayed in augmented reality efficiently through multithreading with the GPU. Designing data structures and building the data pipeline for parsing and transforming binary data produced by nuclear radiation detectors; contributed to the optimization of the processing algorithm to decrease the run time by 10 times. Presenting results at the Monitoring, Technology, & Verification research fair in Ann Arbor to 100+ researchers."
-                }
-            }
+        let experienceData = this.experienceForm.getRawValue();
+        let userData = this.userForm.getRawValue();
+        const returnData = {
+            "url": userData.url,
+            "name": userData.fullName,
+            "gradeLevel": userData.gradeLevel,
+            "major": userData.major,
+            "skills": userData.skills,
+            "experiences": experienceData
         }
-        this.service.uploadCandidateInfo(testData).subscribe(
-            response => {
-                console.log(response);
-                this.submitted = true;
-            },
-            error => {
-                console.log(error);
-            }
-        )
-    }
-
-    newUser() {
-        this.model = new User('', '', {}, [], '');
-    }
-
-    changeBool(val: boolean) {
-        val = !val;
-=======
-        // const returnData = {
-        //     "name": this.model.fullName,
-        //     "gradeLevel": this.model.studentStatus,
-        //     "major": this.model.major,
+        // const testData = {
+        //     "url": "https://www.linkedin.com/jobs/search/?currentJobId=3339978993&f_JT=I&keywords=software%20engineer%20intern",
+        //     "name": "Kimi Weng",
+        //     "gradeLevel": "Sophomore",
+        //     "major": "Computer Science",
         //     "skills": {
         //         "1": "C++",
         //         "2": "Python",
@@ -137,16 +122,18 @@ export class UserFormComponent {
         //         }
         //     }
         // }
-        console.warn(this.userForm.value);
-        // this.service.uploadCandidateInfo(returnData).subscribe(
-        //     response => {
-        //         console.log(response);
-        //         this.submitted = true;
-        //     },
-        //     error => {
-        //         console.log(error);
-        //     }
-        // )
->>>>>>> de79906e9ef65f46b3b5b446638206fc7a844e19
+        this.service.uploadCandidateInfo(returnData).subscribe(
+            response => {
+                console.log(response);
+                this.submitted = true;
+            },
+            error => {
+                console.log(error);
+            }
+        )
+    }
+
+    changeBool(val: boolean) {
+        val = !val;
     }
 }
--- frontend/src/app/user.ts
+++ frontend/src/app/user.ts
@@ -1,11 +1,12 @@
 export class User {
 
     constructor(
+        public url: string,
         public fullName: string,
         public studentStatus: string,
         public major: string,
         public skills: object,
-        public experience: object,
+        public experience: Array<object>,
     ) { }
 
 }
