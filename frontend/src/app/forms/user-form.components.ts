163acd9795ea9ad18b65a351892ab87d9ba5ff8f
Kimi Weng <kimiweng@Kimis-MacBook-Pro.local>
2023-02-05 18:46:05 -0500

    unsure

--- frontend/src/app/forms/user-form.components.ts
+++ frontend/src/app/forms/user-form.components.ts
@@ -51,12 +51,11 @@ export class UserFormComponent {
     onSubmit() { 
         this.submitted = true; 
         console.log("Made it here");
-<<<<<<< HEAD
-        const testData = {
-            "url": "https://www.linkedin.com/jobs/search/?currentJobId=3339978993&f_JT=I&keywords=software%20engineer%20intern",
-            "name": "Kimi Weng",
-            "gradeLevel": "Sophomore",
-            "major": "Computer Science",
+        const returnData = {
+            "url": "https://www.linkedin.com/jobs/search/?currentJobId=3259609035&f_JT=I&keywords=software%20engineer%20intern",
+            "name": this.model.fullName,
+            "gradeLevel": this.model.studentStatus,
+            "major": this.model.major,
             "skills": {
                 "1": "C++",
                 "2": "Python",
@@ -86,7 +85,8 @@ export class UserFormComponent {
                 }
             }
         }
-        this.service.uploadCandidateInfo(testData).subscribe(
+        // console.warn(this.userForm.value);
+        this.service.uploadCandidateInfo(returnData).subscribe(
             response => {
                 console.log(response);
                 this.submitted = true;
@@ -95,58 +95,5 @@ export class UserFormComponent {
                 console.log(error);
             }
         )
-    }
-
-    newUser() {
-        this.model = new User('', '', {}, [], '');
-    }
-
-    changeBool(val: boolean) {
-        val = !val;
-=======
-        // const returnData = {
-        //     "name": this.model.fullName,
-        //     "gradeLevel": this.model.studentStatus,
-        //     "major": this.model.major,
-        //     "skills": {
-        //         "1": "C++",
-        //         "2": "Python",
-        //         "3": "Javascript",
-        //         "4": "Unreal Engine",
-        //         "5": "Rust",
-        //         "6": "SQL",
-        //         "7": "Docker",
-        //         "8": "AWS"
-        //     },
-        //     "experiences": {
-        //         "1": {
-        //             "title": "MES Software Engineer Intern",
-        //             "company": "Yanfeng Automotive Interior Systems Co.",
-        //             "onGoing": false,
-        //             "startDate": "May 2022",
-        //             "endDate": "Jul 2022",
-        //             "description": "Self proposed and led a team of two to develop a web application for parsing multiple 1000+ lines of on-site MES engineers’ troubleshooting logs and generating statistical reports with graphics with Javascript and React.js, which greatly increased the efficiency of the employees. Showcased the product in a department-wide presentation and hosted the application on the company network. Troubleshooting company proprietary manufacturing software with SQL on-site to maintain production line uptime."
-        //         },
-        //         "2": {
-        //             "title": "Research Programmer",
-        //             "company": "Department of Nuclear Engineering, University of Michigan",
-        //             "onGoing": true,
-        //             "startDate": "Sep 2022",
-        //             "endDate": "n/a",
-        //             "description": "Developing AR software for displaying 3D visualizations of radiation signals with C++, Rust, and Unity C#. Iterating on the Marching Cube algorithm that triangulates 3D point cloud data structure and generates a 3D mesh representation that could be displayed in augmented reality efficiently through multithreading with the GPU. Designing data structures and building the data pipeline for parsing and transforming binary data produced by nuclear radiation detectors; contributed to the optimization of the processing algorithm to decrease the run time by 10 times. Presenting results at the Monitoring, Technology, & Verification research fair in Ann Arbor to 100+ researchers."
-        //         }
-        //     }
-        // }
-        console.warn(this.userForm.value);
-        // this.service.uploadCandidateInfo(returnData).subscribe(
-        //     response => {
-        //         console.log(response);
-        //         this.submitted = true;
-        //     },
-        //     error => {
-        //         console.log(error);
-        //     }
-        // )
->>>>>>> de79906e9ef65f46b3b5b446638206fc7a844e19
     }
 }
