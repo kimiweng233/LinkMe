c79821395012720b4e0c1d07aaef1ab174e3dd75
jespieee <mhuhu@umich.edu>
2023-02-27 16:44:11 -0500

    fixed userForm Initialization bug

--- frontend/src/app/forms/user-form.components.ts
+++ frontend/src/app/forms/user-form.components.ts
@@ -1,7 +1,6 @@
-import { Component, Input, ViewChild, AfterViewInit, OnInit } from '@angular/core';
-import { User } from '../user';
+import { Component, OnInit } from '@angular/core';
 import { Service } from 'src/app/services/service';
-import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import { Validators } from '@angular/forms';
 
 @Component({
@@ -10,9 +9,9 @@ import { Validators } from '@angular/forms';
     styleUrls: ['./user-form.component.css']
 })
 
-export class UserFormComponent {
+export class UserFormComponent implements OnInit {
     submitted = false;
-    studentStatuses = ['Freshman', 'Sophomore', 'Junior', 'Senior', 'Graduate'];
+    studentStatuses = ['Freshman', 'Sophomore', 'Junior', 'Senior', 'Graduated'];
 
     skillForm = this.fb.array([
         this.fb.control('')
@@ -24,18 +23,18 @@ export class UserFormComponent {
     })
 
     userForm = this.fb.group({
-        url: [''],
+        url: ['', Validators.required],
         fullName: ['', Validators.required],
         gradeLevel: [''],
         major: [''],
         skills: this.skillForm
     })
 
-    constructor(private service: Service, private fb: FormBuilder) { 
-        // this.experienceForm = this.fb.group({
-        //     experiences: this.fb.array([this.createExperience()])
-        // });
+    ngOnInit(): void {
+        
     }
+
+    constructor(private service: Service, private fb: FormBuilder) {}
         
     createExperience(): FormGroup {
         return this.fb.group({
@@ -45,86 +44,93 @@ export class UserFormComponent {
             endDate: '',
             description: '',
         });
-      }
-    
-      get experienceControls() {
+    }
+
+    get experienceControls() {
         return this.experienceForm.get('experiences')['controls'];
-      }
-    
-      addExperience(): void {
+    }
+
+    addExperience() {
         this.experiences = this.experienceForm.get('experiences') as FormArray;
         this.experiences.push(this.createExperience());
-      }
-    
-      removeExperience(i: number): void {
-        this.experiences.removeAt(i);
-      }
-    // name, student status, major, skills, experience
-    
-    
+    }
 
+    removeExperience(i: number) {
+        this.experiences.removeAt(i);
+    }
     get userFormData() {
         return this.userForm as FormGroup;
     }
     get skills(): FormArray {
         return this.userForm.get('skills') as FormArray;
     }
-    
     addSkill() {
         this.skills.push(this.fb.control(''));
     }
     removeSkill(i: number) {
         this.skills.removeAt(i);
     }
+
     onSubmit() { 
         this.submitted = true; 
         console.log("Made it here");
-        // let experienceData = this.experienceForm.getRawValue();
-        // let userData = this.userForm.getRawValue();
-        // const returnData = {
-        //     "url": userData.url,
-        //     "name": userData.fullName,
-        //     "gradeLevel": userData.gradeLevel,
-        //     "major": userData.major,
-        //     "skills": userData.skills,
-        //     "experiences": experienceData
-        // }
-        const testData = {
-            "url": "https://www.linkedin.com/jobs/search/?currentJobId=3339978993&f_JT=I&keywords=software%20engineer%20intern",
-            "name": "Kimi Weng",
-            "gradeLevel": "Sophomore",
-            "major": "Computer Science",
-            "skills": {
-                "1": "C++",
-                "2": "Python",
-                "3": "Javascript",
-                "4": "Unreal Engine",
-                "5": "Rust",
-                "6": "SQL",
-                "7": "Docker",
-                "8": "AWS"
-            },
-            "experiences": {
-                "1": {
-                    "title": "MES Software Engineer Intern",
-                    "company": "Yanfeng Automotive Interior Systems Co.",
-                    "onGoing": false,
-                    "startDate": "May 2022",
-                    "endDate": "Jul 2022",
-                    "description": "Self proposed and led a team of two to develop a web application for parsing multiple 1000+ lines of on-site MES engineers’ troubleshooting logs and generating statistical reports with graphics with Javascript and React.js, which greatly increased the efficiency of the employees. Showcased the product in a department-wide presentation and hosted the application on the company network. Troubleshooting company proprietary manufacturing software with SQL on-site to maintain production line uptime."
-                },
-                "2": {
-                    "title": "Research Programmer",
-                    "company": "Department of Nuclear Engineering, University of Michigan",
-                    "onGoing": true,
-                    "startDate": "Sep 2022",
-                    "endDate": "n/a",
-                    "description": "Developing AR software for displaying 3D visualizations of radiation signals with C++, Rust, and Unity C#. Iterating on the Marching Cube algorithm that triangulates 3D point cloud data structure and generates a 3D mesh representation that could be displayed in augmented reality efficiently through multithreading with the GPU. Designing data structures and building the data pipeline for parsing and transforming binary data produced by nuclear radiation detectors; contributed to the optimization of the processing algorithm to decrease the run time by 10 times. Presenting results at the Monitoring, Technology, & Verification research fair in Ann Arbor to 100+ researchers."
-                }
-            }
+        let experienceData = this.experienceForm.getRawValue();
+        let exp = experienceData.experiences;
+        let experienceObj = {};
+        for (let i = 0; i < exp.length; i++) {
+            experienceObj[i] = exp.at(i);
         }
+        let userData = this.userForm.getRawValue();
+        let userSkills = userData.skills;
+        let skillsObj = {};
+        for (let i = 0; i < userSkills.length; i++) {
+            skillsObj[i] = userSkills.at(i);
+        }
+        const returnData = {
+            "url": userData.url,
+            "name": userData.fullName,
+            "gradeLevel": userData.gradeLevel,
+            "major": userData.major,
+            "skills": skillsObj,
+            "experiences": experienceObj,
+        }
+        // const testData = {
+        //     "url": "https://www.linkedin.com/jobs/search/?currentJobId=3339978993&f_JT=I&keywords=software%20engineer%20intern",
+        //     "name": "Kimi Weng",
+        //     "gradeLevel": "Sophomore",
+        //     "major": "Computer Science",
+        //     "skills": {
+        //         "1": "C++",
+        //         "2": "Python",
+        //         "3": "Javascript",
+        //         "4": "Unreal Engine",
+        //         "5": "Rust",
+        //         "6": "SQL",
+        //         "7": "Docker",
+        //         "8": "AWS"
+        //     },
+        //     "experiences": {
+        //         "1": {
+        //             "title": "MES Software Engineer Intern",
+        //             "company": "Yanfeng Automotive Interior Systems Co.",
+        //             "onGoing": false,
+        //             "startDate": "May 2022",
+        //             "endDate": "Jul 2022",
+        //             "description": "Self proposed and led a team of two to develop a web application for parsing multiple 1000+ lines of on-site MES engineers’ troubleshooting logs and generating statistical reports with graphics with Javascript and React.js, which greatly increased the efficiency of the employees. Showcased the product in a department-wide presentation and hosted the application on the company network. Troubleshooting company proprietary manufacturing software with SQL on-site to maintain production line uptime."
+        //         },
+        //         "2": {
+        //             "title": "Research Programmer",
+        //             "company": "Department of Nuclear Engineering, University of Michigan",
+        //             "onGoing": true,
+        //             "startDate": "Sep 2022",
+        //             "endDate": "n/a",
+        //             "description": "Developing AR software for displaying 3D visualizations of radiation signals with C++, Rust, and Unity C#. Iterating on the Marching Cube algorithm that triangulates 3D point cloud data structure and generates a 3D mesh representation that could be displayed in augmented reality efficiently through multithreading with the GPU. Designing data structures and building the data pipeline for parsing and transforming binary data produced by nuclear radiation detectors; contributed to the optimization of the processing algorithm to decrease the run time by 10 times. Presenting results at the Monitoring, Technology, & Verification research fair in Ann Arbor to 100+ researchers."
+        //         }
+        //     }
+        // }
         // console.warn(this.userForm.value);
-        this.service.uploadCandidateInfo(testData).subscribe(
+        console.log(returnData);
+        this.service.uploadCandidateInfo(returnData).subscribe(
             response => {
                 console.log(response);
                 this.submitted = true;
@@ -133,9 +139,5 @@ export class UserFormComponent {
                 console.log(error);
             }
         )
-    }
-
-    changeBool(val: boolean) {
-        val = !val;
     }
 }
