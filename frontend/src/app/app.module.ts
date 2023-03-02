4c70e6d89151f614eec9893be09b0b4c7dc7020f
jespieee <mhuhu@umich.edu>
2023-03-02 12:30:37 -0500

    fixing gitignores

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
 import { TokenInterceptor } from "./token.interceptor";
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MdbDropdownModule } from 'mdb-angular-ui-kit/dropdown'
 
 const appRoute: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
@@ -35,6 +36,7 @@ const appRoute: Routes = [
     RouterModule.forRoot(appRoute),
     FontAwesomeModule,
     RouterModule,
+    MdbDropdownModule,
   ],
   declarations: [
     AppComponent,
